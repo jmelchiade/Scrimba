d71184ee3687f9764de35f2c15ddb6cf45e9e1a1
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-18 08:48:18 -0400

    Created multiple components for page including Header, MainContent, and Footer

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function Footer(){
+    return(
+        <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
+    )
+}
+
+function MainContent(){
+    return(
+        <div>
+        <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol></div>
+    )
+}
+
 function Page() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src="./react-logo.png" width="40px" />
-                </nav>
-            </header>
-            <h1>This is Practice for React</h1>
-            <h2>Reasons to learn React:</h2>
-            <ol>
-                <li>It's a resourceful, progressive tool for web developers </li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
-            <footer>
-                <small>© 2022 Melchiade development. All rights reserved.</small>
-            </footer>
+            <Header />
+            <MainContent/>
+            <Footer/>
         </div>
     )
 }
+
+
 
 ReactDOM.render(<Page />, document.getElementById("root"))
