91807be8f90e5c1173680287172670b2a2e619fd
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-10-18 20:01:04 -0400

    Revised header to include a nav list. Revised styling for overall page.

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-/**
-Challenge: 
-
-- Move the `footer` into its own component called "Footer" 
-  and render that component inside the Page component.
-- Move the `h1` and `ol` together into another component
-  called "MainContent" and render inside Page as well.
-*/
 
 function Header() {
     return (
         <header>
-            <nav>
-                <img src="./react-logo.png" width="40px" />
+             <nav className="nav">
+                <img src="./react-logo.png" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>
     )
@@ -33,8 +30,7 @@ function MainContent(){
         <div>
         <h1>Reasons I'm excited to learn React</h1>
             <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
+                <li>It's a resourceful tool, used in modern web-development offices</li>
                 <li>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol></div>
